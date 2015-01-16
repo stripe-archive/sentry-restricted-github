c5494ecc68c50044b580958be996c8dfe85df0ab
Daniel Benamy <dbenamy@stripe.com>
2015-01-16 15:36:09 -0800

    Support 2 interfaces for exceptions

--- src/sentry_safe_github/plugin.py
+++ src/sentry_safe_github/plugin.py
@@ -47,7 +47,8 @@ class SafeGithubPlugin(Plugin):
         })
 
     def _get_group_body(self, request, group, event, **kwargs):
-        interface = event.interfaces.get('exception')
+        interface = (event.interfaces.get('exception') or
+                     event.interfaces.get('sentry.interfaces.Exception'))
         if interface:
             return safe_execute(interface.to_string, event)
         return
